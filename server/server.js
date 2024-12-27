6e60f2aeb2a1debb8f7743bea2c07179870ddbb5
anastasia <anyasirman@outlook.com>
2024-12-27 13:50:53 -0500

    added new links

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const path = require('path')
 app.use(express.json())
-app.use(cors())
+    app.use(cors({
+        origin: 'https://spaced-repetition-883p.onrender.com'
+      }));
 
 
 
